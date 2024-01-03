6c61d1898f06d5fd7b1d548b85a95f974fdf468f
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-03 15:57:08 -0600

    Orienting the API to the domain with MapStruct

---
+++ src/main/java/dev/victorvazquez/market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package dev.victorvazquez.market.persistence.mapper;
+
+import dev.victorvazquez.market.domain.Category;
+import dev.victorvazquez.market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "description", target = "category"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
