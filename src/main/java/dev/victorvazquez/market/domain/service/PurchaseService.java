2d1492f3c05537461d0dfffae6a0d48719bf2c73
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2024-01-03 19:49:50 -0600

    Solving errors

---
+++ src/main/java/dev/victorvazquez/market/domain/service/PurchaseService.java
@@ -0,0 +1,27 @@
+package dev.victorvazquez.market.domain.service;
+
+import dev.victorvazquez.market.domain.Purchase;
+import dev.victorvazquez.market.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+  @Autowired
+  private PurchaseRepository purchaseRepository;
+
+  public List<Purchase> getAll(){
+    return purchaseRepository.getAll();
+  }
+
+  public Optional<List<Purchase>> getByClient(String clientId){
+    return purchaseRepository.getByClient(clientId);
+  }
+
+  public Purchase save(Purchase purchase) {
+    return purchaseRepository.save(purchase);
+  }
+}
